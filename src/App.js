e62e88b2ab4d30edb46886bd9559b644c0b5b089
Shahith R <shahithr99@gmail.com>
2022-08-19 14:23:36 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [data, setData] = useState({});
+  const [value, setValue] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setValue((prev) => prev + 1);
+    }, 120000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  useEffect(() => {
+    if(value > 0){
+      fetch(`https://jsonplaceholder.typicode.com/todos/${value}`)
+      .then(response => response.json())
+      .then(json => setData(json))
+    }
+  },[value])
+
+  const handleApi = () => {
+    setValue((prev) => prev + 1);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {Object.keys(data).length > 0 && <h1>{data.title}</h1>}
+      <button onClick={handleApi}>Fetch API</button>
     </div>
   );
 }
